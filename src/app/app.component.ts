20af0ba0d2f75f37d2e5508bf01bcec97229c154
Felix Chantrel <felchantrel@gmail.com>
2018-11-04 16:32:21 +0100

    Sauvegarde et chargement des quizs ok.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,12 +15,16 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
+    private quizsProv: QuizsProvider) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+
+      this.quizsProv.loadFromStorage().then(() => {
+        statusBar.styleDefault();
+        splashScreen.hide();
+      });
     });
   }
 }
