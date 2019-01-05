d374fef65b9cb0d3bce7228a50b5db8bd32343e8
Felix Chantrel <felchantrel@gmail.com>
2019-01-05 17:07:41 +0100

    Debut d'integration des questions rightPicture dans Play.

--- src/pages/play/play.ts
+++ src/pages/play/play.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { File } from '@ionic-native/file';
 import { trigger, keyframes, style, animate, transition } from '@angular/animations';
 
 import { Quiz } from '../../models/quiz';
@@ -39,17 +40,6 @@ enum ScreenStateType {
         ]),
       ]),
       trigger(
-      'answerAnimation' , [
-        transition(':enter', [
-          style({transform: 'rotate3d(1, 1, 1, -90deg)', opacity: 0}),
-          animate('600ms', style({transform: 'none', opacity: 1}))
-        ]),
-        transition(':leave', [
-          style({transform: 'none', opacity: 1}),
-          animate('600ms', style({transform: 'rotate3d(1, 1, 1, -90deg)', opacity: 0}))
-        ]),
-      ]),
-      trigger(
       'timeBarAnimation' , [
         transition(':enter', [
           style({opacity: 0}),
@@ -62,49 +52,93 @@ enum ScreenStateType {
           )
         ])
       ]),
+      trigger(
+      'answerAnimation' , [
+        transition(':enter', [
+          style({transform: 'rotate3d(1, 1, 1, -90deg)', opacity: 0}),
+          animate('600ms', style({transform: 'none', opacity: 1}))
+        ]),
+        transition(':leave', [
+          style({transform: 'none', opacity: 1}),
+          animate('600ms', style({transform: 'rotate3d(1, 1, 1, -90deg)', opacity: 0}))
+        ]),
+      ]),
+      trigger(
+      'pictureAnimation' , [
+        transition(':enter', [
+          style({transform: 'rotate3d(1, 1, 0.5, -360deg)', opacity: 1}),
+          animate('12000ms', style({transform: 'none', opacity: 1}))
+        ]),
+        transition(':leave', [
+          style({transform: 'none', opacity: 1}),
+          animate('12000ms', style({transform: 'rotate3d(0, 1, 0, -360deg)', opacity: 1}))
+        ]),
+      ]),
   ],
   templateUrl: 'play.html'
 })
 
 export class PlayPage {
   private ScreenStateType = ScreenStateType; //for use in Angluar html
+  private QuestionType = QuestionType; //for use in Angular html
   private quiz: Quiz;
   private currentCategory: number;
+  private currentQuestions: Array<Question>;
   private currentQuestion: number;
   private screenState: ScreenStateType;
 
-  constructor(public navCtrl: NavController,params: NavParams) {
+  constructor(public navCtrl: NavController, private file: File, params: NavParams) {
     this.quiz = params.data.quiz;
 
-    this.currentCategory = 0;
-    this.currentQuestion = 0;
+    if (!this.quiz) {
+      this.navCtrl.pop();
+    }
+    else {
+      if (this.quiz.categorys.length < 1 || this.quiz.questions.length < 1) {
+        this.navCtrl.pop();
+      }
+      else {
+        this.currentCategory = 0;
+        this.currentQuestion = 0;
+        this.currentQuestions = this.getQuestionsFromCategory(this.quiz.categorys[this.currentCategory]);
 
-    this.screenState = ScreenStateType.displayTitle;
+        this.screenState = ScreenStateType.displayTitle;
+      }
+    }
   }
 
   next() {
-    /*if (this.screenState === ScreenStateType.displayTitle) {
+    if (this.screenState === ScreenStateType.displayTitle) {
       this.screenState = ScreenStateType.displayCategoryTitle;
     }
     else if (this.screenState === ScreenStateType.displayCategoryTitle) {
       this.screenState = ScreenStateType.displayQuestion;
     }
     else if (this.screenState === ScreenStateType.displayQuestion) {
-      if (this.currentQuestion < this.quiz.categorys[this.currentCategory].questions.length - 1) {
+      if (this.currentQuestion < this.currentQuestions.length - 1) {
         this.currentQuestion++;
       }
       else {
         if (this.currentCategory < this.quiz.categorys.length - 1) {
           this.currentCategory++;
-          this.currentQuestion = 0;
           this.screenState = ScreenStateType.displayCategoryTitle;
+
+          this.currentQuestion = 0;
+          this.currentQuestions = this.getQuestionsFromCategory(this.quiz.categorys[this.currentCategory]);
         }
         else {
           this.navCtrl.pop();
         }
       }
-    }*/
+    }
+  }
 
+  getQuestionsFromCategory(category: Category) {
+    return this.quiz.questions.filter((question) => question.category.name === category.name);
+  }
+
+  getAttachamentsDir(questionIndex: number) {
+    return this.file.dataDirectory + this.quiz.uuid + '/' + this.currentQuestions[questionIndex].uuid + '/';
   }
 
 }
