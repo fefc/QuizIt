f4eae4c9d605a327e217926d6c548362b512f066
Felix Chantrel <felchantrel@gmail.com>
2019-06-20 19:31:01 +0930

    Some tests on the about page, should be deleted.

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -2,12 +2,16 @@ import { Component, ViewChild } from '@angular/core';
 import { ViewController, Slides } from 'ionic-angular';
 import { AppVersion } from '@ionic-native/app-version';
 
+//declare var presentation: any;
+
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
   private versionNumber: string;
+
+  private presentation: any;
 
   constructor(public viewCtrl: ViewController, private appVersion: AppVersion) {
     if (appVersion) {
@@ -18,6 +22,26 @@ export class AboutPage {
         this.versionNumber = "Unknown";
       });
     }
+
+    this.presentation = (<any>window).Presentation
+
+    if ((<any>window).navigator) {
+      alert("HAPPY");
+
+      (<any>window).navigator.presentation.onavailablechange = function(dict){
+          if(dict.available){
+              alert("available");
+
+              (<any>window).navigator.presentation.requestSession("assets/httpd/indexs.html");
+          } else {
+              alert("non available");
+          }
+      };
+    } else {
+      alert("PAS HAPPY");
+    }
+
+
   }
 
   dismiss() {
