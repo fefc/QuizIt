5e3ec9e41b76a5b7f7279cf9e8c41fafa4591536
Felix Chantrel <felchantrel@gmail.com>
2020-04-15 20:39:31 +0200

    Beta tests for a good service-worker with storage.

--- src/service-worker.js
+++ src/service-worker.js
@@ -11,6 +11,8 @@ self.toolbox.options.cache = {
   name: 'ionic-cache'
 };
 
+self.toolbox.options.debug = false;
+
 // pre-cache our key assets
 self.toolbox.precache(
   [
@@ -29,3 +31,62 @@ self.toolbox.router.any('/*', self.toolbox.fastest);
 // for any other requests go to the network, cache,
 // and then only use that cached resource if your user goes offline
 self.toolbox.router.default = self.toolbox.networkFirst;
+
+// for any requests from firebasestorage that are actual files (token is given)
+// use cache first
+self.toolbox.router.any(/^.*firebasestorage.*token=.*$/, async function(request, values, options) {
+  try {
+    if (request.headers.get('range')) {
+      var pos = Number(/^bytes\=(\d+)\-$/g.exec(request.headers.get('range'))[1]);
+      console.log('Range request for', request.url, ', starting position:', pos);
+
+      let response = await caches.match(request);
+
+      if (response) {
+        console.log('Found response in cache:', response);
+        return response;
+      } else {
+        console.log('No response found in cache. About to fetch from network...');
+
+        self.toolbox.cache(request.url, {}).then(() => {
+          console.log('Cached file')
+        }).catch((error) => {
+          console.log('Could not cache: ', error);
+        });
+
+        response = await fetch(request);
+
+        console.log('Response from network is:', response);
+
+        return response;
+      }
+
+    } else {
+      console.log('Non-range request for', request.url);
+      
+      let response = await caches.match(request);
+
+      if (response) {
+        console.log('Found response in cache:', response);
+        return response;
+      } else {
+        console.log('No response found in cache. About to fetch from network...');
+
+        self.toolbox.cache(request.url, {}).then(() => {
+          console.log('Cached file')
+        }).catch((error) => {
+          console.log('Could not cache: ', error);
+        });
+
+        response = await fetch(request);
+        console.log('Response from network is:', response);
+
+        return response;
+      }
+    }
+  } catch (error) {
+    console.error('Fetching failed:', error);
+
+    throw error;
+  }
+});
